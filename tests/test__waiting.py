654200acc90a9ae2a9d108d661678e3980ac0073
Rotem Yaari <vmalloc@gmail.com>
2012-03-07 15:35:29 +0200

    add iterwait function to return control to the user after each wait iteration

--- tests/test__waiting.py
+++ tests/test__waiting.py
@@ -1,9 +1,9 @@
 from forge import ForgeTestCase
 import waiting
 
-class WaitingTest(ForgeTestCase):
+class VirtualTimeTest(ForgeTestCase):
     def setUp(self):
-        super(WaitingTest, self).setUp()
+        super(VirtualTimeTest, self).setUp()
         self.virtual_time = 0
         self.sleeps_performed = []
         self.predicate_satisfied = False
@@ -12,18 +12,20 @@ class WaitingTest(ForgeTestCase):
         self.forge.replace_with(waiting, "_sleep", self._sleep)
         self.forge.replace_with(waiting, "_time", self._time)
         self.forge.replace_with(waiting.deadlines, "_time", self._time)
-    def predicate(self):
-        if self.satisfy_at_time is not None and self.satisfy_at_time == self.virtual_time:
-            self.predicate_satisfied = True
-        if self.satisfy_after_time is not None and self.satisfy_after_time <= self.virtual_time:
-            self.predicate_satisfied = True
-        return self.predicate_satisfied
     def _sleep(self, delta):
         self.sleeps_performed.append(delta)
         self.virtual_time += delta
         self.assertGreater(1000, len(self.sleeps_performed), "Infinite loop")
     def _time(self):
         return self.virtual_time
+
+class WaitingTest(VirtualTimeTest):
+    def predicate(self):
+        if self.satisfy_at_time is not None and self.satisfy_at_time == self.virtual_time:
+            self.predicate_satisfied = True
+        if self.satisfy_after_time is not None and self.satisfy_after_time <= self.virtual_time:
+            self.predicate_satisfied = True
+        return self.predicate_satisfied
     def test__waiting_does_not_expire(self):
         num_tries = 9
         sleep_seconds = 10
@@ -33,19 +35,36 @@ class WaitingTest(ForgeTestCase):
         with self.assertRaises(waiting.TimeoutExpired):
             waiting.wait(self.predicate, sleep_seconds=3, timeout_seconds=5) # timeout_seconds intentionally not divided by sleep
         self.assertEquals(self.virtual_time, 5)
+    def test__iterwait_sleep_time(self):
+        sleep_seconds = 5
+        num_sleeps = 7
+        self.satisfy_at_time = sleep_seconds * num_sleeps
+        previous_time = self.virtual_time
+        for retry in waiting.iterwait(self.predicate, sleep_seconds=sleep_seconds):
+            self.assertEquals(self.virtual_time, previous_time)
+            previous_time = self.virtual_time + sleep_seconds
+            self._sleep(1.5) # shouldn't affect the sleep
+        self.assertEquals(self.virtual_time, num_sleeps * sleep_seconds)
 
-class AggregationTest(ForgeTestCase):
+class AggregationTest(VirtualTimeTest):
     def setUp(self):
         super(AggregationTest, self).setUp()
-        self.forge.replace(waiting, "_sleep")
         self.predicates = [self.forge.create_wildcard_mock() for i in range(5)]
     def test__wait_any(self):
         for p in self.predicates:
             p().and_return(False)
-        waiting._sleep(1)
         self.predicates[0]().and_return(True)
         self.forge.replay()
         waiting.wait(waiting.ANY(self.predicates))
+    def test__wait_any_satisfied_on_start(self):
+        for index, p in enumerate(self.predicates):
+            works = (index == 2)
+            p().and_return(works)
+            if works:
+                break
+        self.forge.replay()
+        waiting.wait(waiting.ANY(self.predicates))
+        self.assertEquals(self.virtual_time, 0)
     def test__wait_all(self):
         for iteration in xrange(len(self.predicates) * 2, 0, -1):
             iteration -= 1
@@ -53,7 +72,6 @@ class AggregationTest(ForgeTestCase):
                 if index > iteration:
                     continue
                 p().and_return(index == iteration)
-            if iteration != 0:
-                waiting._sleep(1)
         self.forge.replay()
         waiting.wait(waiting.ALL(self.predicates))
+        self.assertEquals(self.virtual_time, len(self.predicates) * 2 - 1)
--- waiting/__init__.py
+++ waiting/__init__.py
@@ -1,18 +1,37 @@
 from .__version__ import __version__
+from contextlib import contextmanager
 from time import sleep as _sleep
 from time import time as _time
 from .deadlines import make_deadline as _make_deadline
 from .exceptions import TimeoutExpired
 
-def wait(predicate, timeout_seconds=None, sleep_seconds=1):
+def wait(*args, **kwargs):
+    result = _Result()
+    for x in iterwait(result=result, *args, **kwargs):
+        pass
+    return result.result
+
+def iterwait(predicate, timeout_seconds=None, sleep_seconds=1, result=None):
     timeout = _make_deadline(timeout_seconds)
+    if result is None:
+        result = _Result()
     while True:
-        result = predicate()
-        if result:
-            return result
+        result.result = predicate()
+        if result.result:
+            return
         if timeout.is_expired():
             raise TimeoutExpired()
-        _sleep(_get_sleep_time(timeout, sleep_seconds))
+        with _end_sleeping(_get_sleep_time(timeout, sleep_seconds)):
+            yield
+
+@contextmanager
+def _end_sleeping(total_seconds):
+    deadline = _make_deadline(total_seconds)
+    yield
+    _sleep(max(0, deadline.get_num_seconds_remaining()))
+
+class _Result(object):
+    result = None
 
 def _get_sleep_time(timeout, sleep_seconds):
     time_remaining = timeout.get_num_seconds_remaining()

d513ddc32484f0b288a8b0678cfebc9ae243b287
wiggin15 <wiggin15@yahoo.com>
2016-02-13 09:51:59 +0200

    remove package dependcy on pyforge and unittest2
    unittest2 is not used as far as I can see
    pyforge is only required for tests, not for installation

--- setup.py
+++ setup.py
@@ -1,14 +1,8 @@
 import os
-import platform
-import itertools
 from setuptools import setup, find_packages
 
 with open(os.path.join(os.path.dirname(__file__), "waiting", "__version__.py")) as version_file:
     exec(version_file.read())
-
-_INSTALL_REQUIREMENTS = ["pyforge"]
-if platform.python_version() < '2.7':
-    _INSTALL_REQUIREMENTS.append('unittest2')
 
 setup(name="waiting",
       classifiers = [
@@ -24,7 +18,8 @@ setup(name="waiting",
       author_email="vmalloc@gmail.com",
       version=__version__,
       packages=find_packages(exclude=["tests"]),
-      install_requires=_INSTALL_REQUIREMENTS,
+      install_requires=[],
+      tests_require=["pyforge"],
       scripts=[],
       namespace_packages=[]
       )

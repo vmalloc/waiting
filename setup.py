4cd5aecc570b80fe6828d6baeee0412b3525a622
Ayala Shachar <shachar.ayala@gmail.com>
2020-10-05 19:14:40 +0300

    Update supported python versions (>= 3.5)

--- setup.py
+++ setup.py
@@ -6,11 +6,10 @@ with open(os.path.join(os.path.dirname(__file__), "waiting", "__version__.py")) 
 
 setup(name="waiting",
       classifiers = [
-          "Programming Language :: Python :: 2.6",
-          "Programming Language :: Python :: 2.7",
-          "Programming Language :: Python :: 3.3",
-          "Programming Language :: Python :: 3.4",
           "Programming Language :: Python :: 3.5",
+          "Programming Language :: Python :: 3.6",
+          "Programming Language :: Python :: 3.7",
+          "Programming Language :: Python :: 3.8",
           ],
       description="Utility for waiting for stuff to happen",
       license="BSD",
